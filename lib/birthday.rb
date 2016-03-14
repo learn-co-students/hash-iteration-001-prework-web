d03b4d240e22d3092ab80ceeac68494de6ecf800
wengong <wentoshare@gmail.com>
2016-03-14 23:02:19 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,10 +1,11 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
+  birthday_kids.each {|a,b| puts "Happy Birthday #{a}! You are now #{b} years old!" }
   # add your code snippet here!
 end
 
