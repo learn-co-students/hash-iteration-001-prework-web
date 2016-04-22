b149344b4624f70a7dc15e0c0a399b752adb6acf
KevinDTran <kevin@zipmark.com>
2016-04-22 07:33:51 -0500

    Completed

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,7 +5,14 @@
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
+winner = ""
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+      return winner
+    end
+  end
 end
+
