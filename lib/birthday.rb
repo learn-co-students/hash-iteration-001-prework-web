1945a281a755c86695f3f3cedd7be1c9643b985d
Satu Barnhill <satu.barnhill@gmail.com>
2016-07-02 01:15:59 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each do |kid, age|
-    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  birthday_kids.each do |kiddo, age|
+    puts "Happy Birthday #{kiddo}! You are now #{age} years old!"
   end
 end
